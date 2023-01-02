911333794bbf6d398e61ab682b0a090366c69226
Urmila More <moreurmila2111@gmail.com>
2023-01-02 23:51:59 +0530

    problems

---
+++ gitHubproject/src/gitpackage/ABCD.java
@@ -0,0 +1,14 @@
+package gitpackage;
+
+public class ABCD 
+{
+	
+   public static void main(String[] args) 
+   {
+	
+	   System.out.println(" Always i  am ready to face problems ");
+	   
+	   
+	   
+   }
+}
